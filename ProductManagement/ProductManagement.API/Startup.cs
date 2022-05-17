01f80222d7d134cc29d218635504a6913b6f4eaf
ToBeGhost <mikroajans2005@gmail.com>
2022-05-17 23:00:10 +0300

    Added services, swagger ui, database context and auto mapper

--- ProductManagement/ProductManagement.API/Startup.cs
+++ ProductManagement/ProductManagement.API/Startup.cs
@@ -2,10 +2,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using PM.Domain.Data;
+using PM.Services.Catalog;
+using PM.Services.Customers;
+using PM.Services.Directory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,21 +32,56 @@ namespace ProductManagement.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //All auto mapping is done by the AutoMapper.
+            services.AddAutoMapper(typeof(Startup));
+
+            //Implement application database context.
+            services.AddDbContext<PMDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("PMDbConnection")));
+
+            //Implement services.
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<ICountryService, CountryService>();
+            services.AddScoped<ICurrencyService, CurrencyService>();
+            services.AddScoped<IStateProvinceService, StateProvinceService>();
+
+            //Swagger documentation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Product Management", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //using (var migrationSvcScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            //{
+            //    migrationSvcScope.ServiceProvider.GetService<PMDbContext>().Database.Migrate();
+            //}
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //Swagger user interface configuration
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Management v1");
+            });
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
