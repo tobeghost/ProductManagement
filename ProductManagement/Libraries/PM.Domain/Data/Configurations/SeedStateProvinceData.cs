9f5212e7e99ee1e128103f76bcb3fd238adc84c8
ToBeGhost <mikroajans2005@gmail.com>
2022-05-21 09:10:46 +0300

    Added sample data for state province entity

---
+++ ProductManagement/Libraries/PM.Domain/Data/Configurations/SeedStateProvinceData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PM.Domain.Directory;
+
+namespace PM.Domain.Data.Configurations
+{
+    public class SeedStateProvinceData : IEntityTypeConfiguration<StateProvince>
+    {
+        public void Configure(EntityTypeBuilder<StateProvince> builder)
+        {
+            builder.HasData(new StateProvince()
+            {
+                Id = 1,
+                CountryId = 1,
+                Name = "Antalya",
+                Active = true,
+            });
+
+            builder.HasData(new StateProvince()
+            {
+                Id = 2,
+                CountryId = 1,
+                Name = "İstanbul",
+                Active = true,
+            });
+
+            builder.HasData(new StateProvince()
+            {
+                Id = 3,
+                CountryId = 1,
+                Name = "İzmir",
+                Active = true,
+            });
+        }
+    }
+}
---
+++ ProductManagement/Libraries/PM.Domain/Migrations/20220521061032_InitialStateProvincesSampleData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PM.Domain.Migrations
+{
+    public partial class InitialStateProvincesSampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "StateProvinces",
+                columns: new[] { "Id", "Active", "CountryId", "Name" },
+                values: new object[] { 1, true, 1, "Antalya" });
+
+            migrationBuilder.InsertData(
+                table: "StateProvinces",
+                columns: new[] { "Id", "Active", "CountryId", "Name" },
+                values: new object[] { 2, true, 1, "İstanbul" });
+
+            migrationBuilder.InsertData(
+                table: "StateProvinces",
+                columns: new[] { "Id", "Active", "CountryId", "Name" },
+                values: new object[] { 3, true, 1, "İzmir" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "StateProvinces",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StateProvinces",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StateProvinces",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
