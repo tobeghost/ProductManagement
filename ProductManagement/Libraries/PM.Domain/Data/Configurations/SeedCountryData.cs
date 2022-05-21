de33cf747f73e3a569c6132a6343fd571215cb22
ToBeGhost <mikroajans2005@gmail.com>
2022-05-21 09:06:16 +0300

    Added sample data for country entity

---
+++ ProductManagement/Libraries/PM.Domain/Data/Configurations/SeedCountryData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PM.Domain.Directory;
+
+namespace PM.Domain.Data.Configurations
+{
+    public class SeedCountryData : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasData(new Country()
+            {
+                Id = 1,
+                Name = "Türkiye",
+                TwoLetterIsoCode = "TR",
+                Active = true,
+            });
+
+            builder.HasData(new Country()
+            {
+                Id = 2,
+                Name = "Denmark",
+                TwoLetterIsoCode = "DK",
+                Active = true,
+            });
+
+            builder.HasData(new Country()
+            {
+                Id = 3,
+                Name = "Sweden",
+                TwoLetterIsoCode = "SE",
+                Active = true,
+            });
+        }
+    }
+}
---
+++ ProductManagement/Libraries/PM.Domain/Migrations/20220521060551_InitialCountriesSampleData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PM.Domain.Migrations
+{
+    public partial class InitialCountriesSampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Active", "Name", "TwoLetterIsoCode" },
+                values: new object[] { 1, true, "Türkiye", "TR" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Active", "Name", "TwoLetterIsoCode" },
+                values: new object[] { 2, true, "Denmark", "DK" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Active", "Name", "TwoLetterIsoCode" },
+                values: new object[] { 3, true, "Sweden", "SE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
