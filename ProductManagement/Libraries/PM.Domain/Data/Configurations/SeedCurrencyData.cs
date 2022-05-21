8f3cbfedfc43debce7ac344c81bb182e86b03169
ToBeGhost <mikroajans2005@gmail.com>
2022-05-21 09:08:27 +0300

    Added sample data for currency entity

---
+++ ProductManagement/Libraries/PM.Domain/Data/Configurations/SeedCurrencyData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PM.Domain.Directory;
+
+namespace PM.Domain.Data.Configurations
+{
+    public class SeedCurrencyData : IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder.HasData(new Currency()
+            {
+                Id = 1,
+                Name = "Türk Lirası",
+                CurrencyCode = "TRY",
+                NumberDecimal = 2,
+                Rate = 0,
+                Active = true,
+            });
+
+            builder.HasData(new Currency()
+            {
+                Id = 2,
+                Name = "Amerikan Doları",
+                CurrencyCode = "USD",
+                NumberDecimal = 2,
+                Rate = 0,
+                Active = true,
+            });
+        }
+    }
+}
---
+++ ProductManagement/Libraries/PM.Domain/Migrations/20220521060814_InitialCurrenciesSampleData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PM.Domain.Migrations
+{
+    public partial class InitialCurrenciesSampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "Id", "Active", "CurrencyCode", "Name", "NumberDecimal", "Rate" },
+                values: new object[] { 1, true, "TRY", "Türk Lirası", 2, 0m });
+
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "Id", "Active", "CurrencyCode", "Name", "NumberDecimal", "Rate" },
+                values: new object[] { 2, true, "USD", "Amerikan Doları", 2, 0m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
