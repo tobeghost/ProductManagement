0c1cc0fdca3ca16118532d5496b08a30e4ac602f
ToBeGhost <mikroajans2005@gmail.com>
2022-05-21 09:20:56 +0300

    Added sample data for product entity

---
+++ ProductManagement/Libraries/PM.Domain/Data/Configurations/SeedProductData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PM.Domain.Catalog;
+
+namespace PM.Domain.Data.Configurations
+{
+    public class SeedProductData : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product()
+            {
+                Id = 1,
+                Name = "US Polo Assn Gri Erkek T-Shirt",
+                Sku = "1111111",
+                ShortDescription = "%100 Pamuk / Renk Gri",
+                Quantity = 10,
+                Price = 130.62M,
+                OldPrice = 140,
+                CatalogPrice = 130,
+                CostPrice = 100,
+                CurrencyId = 1,
+                Active = true,
+            });
+
+            builder.HasData(new Product()
+            {
+                Id = 2,
+                Name = "Madmext Erkek Siyah Polo Yaka Tişört",
+                Sku = "22222222",
+                ShortDescription = "Numune Bedeni : L Model Ölçüleri : Boy 1.91 / Kilo 88 Ürünün Kalıbı : Slim Fit Ürün İçeri : %100 Cotton Yıkama Talimatı : 30' Derecede Yıkayınız MADE IN TURKEY",
+                Quantity = 15,
+                Price = 123.49M,
+                OldPrice = 150,
+                CatalogPrice = 125,
+                CostPrice = 105.5M,
+                CurrencyId = 1,
+                Active = true,
+            });
+
+            builder.HasData(new Product()
+            {
+                Id = 3,
+                Name = "Apple iPhone 11 64GB Siyah Cep Telefonu",
+                Sku = "333333333",
+                ShortDescription = "(Apple Türkiye Garantili) Aksesuarsız Kutu",
+                Quantity = 100,
+                Price = 11991.55M,
+                OldPrice = 12000,
+                CatalogPrice = 11500,
+                CostPrice = 10000,
+                CurrencyId = 1,
+                Active = true,
+            });
+        }
+    }
+}
---
+++ ProductManagement/Libraries/PM.Domain/Migrations/20220521062050_InitialProductsSampleData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PM.Domain.Migrations
+{
+    public partial class InitialProductsSampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Active", "CatalogPrice", "CostPrice", "CurrencyId", "FullDescription", "Gtin", "Name", "OldPrice", "Price", "Quantity", "ShortDescription", "Sku" },
+                values: new object[] { 1, true, 130m, 100m, 1, null, null, "US Polo Assn Gri Erkek T-Shirt", 140m, 130.62m, 10, "%100 Pamuk / Renk Gri", "1111111" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Active", "CatalogPrice", "CostPrice", "CurrencyId", "FullDescription", "Gtin", "Name", "OldPrice", "Price", "Quantity", "ShortDescription", "Sku" },
+                values: new object[] { 2, true, 125m, 105.5m, 1, null, null, "Madmext Erkek Siyah Polo Yaka Tişört", 150m, 123.49m, 15, "Numune Bedeni : L Model Ölçüleri : Boy 1.91 / Kilo 88 Ürünün Kalıbı : Slim Fit Ürün İçeri : %100 Cotton Yıkama Talimatı : 30' Derecede Yıkayınız MADE IN TURKEY", "22222222" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Active", "CatalogPrice", "CostPrice", "CurrencyId", "FullDescription", "Gtin", "Name", "OldPrice", "Price", "Quantity", "ShortDescription", "Sku" },
+                values: new object[] { 3, true, 11500m, 10000m, 1, null, null, "Apple iPhone 11 64GB Siyah Cep Telefonu", 12000m, 11991.55m, 100, "(Apple Türkiye Garantili) Aksesuarsız Kutu", "333333333" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
